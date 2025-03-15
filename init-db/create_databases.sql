a276b9ad367d273ce6e2a1b792300b1a7459f245
Daniel <jhonpaco@frba.utn.edu.ar>
2025-03-15 23:50:58 +0000

    mas privilegios por las dudas

--- init-db/create_databases.sql
+++ init-db/create_databases.sql
@@ -3,3 +3,8 @@ go
 CREATE USER 'root'@'172.18.0.1' IDENTIFIED BY '12345678';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.18.0.1' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+go 
+create user 'root'@'172.19.0.1' identified by '12345678';
+grant all privileges ON *.* to 'root'@'172.19.0.1' with grant option;
+flush privileges;
+go
